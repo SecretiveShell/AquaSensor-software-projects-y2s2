2935f5caa5c8bc762e7afd927056557d76573a44
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-02-21 12:32:44 +0000

    optimise logic

--- src/frontend/eSmoothing.js
+++ src/frontend/eSmoothing.js
@@ -1,33 +1,38 @@
 function eSmoothing(x) {
-  if (
-    x[0] == 0 ||
-    x[0] > 9000 ||
-    x[x.length - 1] == 0 ||
-    x[x.length - 1] > 9000
-  ) {
-    let t = 0;
-    let n = 0;
-    for (a = 0; a < x.length; a++) {
-      if (x[a] != 0 && x[a] != NaN && x[a] < 9000) {
-        t += Number(x[a]);
-        ++n;
+  const n = x.length;
+
+  // Check if first or last elements need replacing
+  if (x[0] === 0 || x[0] > 9000 || x[n - 1] === 0 || x[n - 1] > 2048) {
+    let sum = 0, count = 0;
+    
+    x.forEach(val => {
+      if (val !== 0 && val < 9000) {
+        sum += val;
+        count++;
+      }
+    });
+
+    const avg = sum / count || 0;
+
+    if (x[0] === 0) x[0] = avg;
+    if (x[n - 1] === 0) x[n - 1] = avg;
+  }
+
+  // Interpolate missing values
+  for (let i = 1; i < n; i++) {
+    if (x[i] === 0 || x[i] > 9000) {
+      let g = 1;
+      while (i + g < n && (x[i + g] === 0 || x[i + g] > 2048)) g++; 
+
+      if (i + g < n) {
+        let diff = (x[i + g] - x[i - 1]) / (g + 1);
+        for (let j = 0; j < g; j++) {
+          x[i + j] = x[i + j - 1] + diff;
+        }
+        i += g - 1; // Skip processed indices
       }
     }
-    let avg = t / n;
-    if (x[0] == 0) x[0] = avg;
-    if (x[x.length - 1] == 0) x[x.length - 1] = avg;
   }
-  for (i = 1; i < x.length; i++) {
-    if (x[i] == 0 || x[i] > 9000) {
-      let g = 0;
-      while (x[i + g] == 0 || x[i + g] > 9000) ++g;
-      let dif = x[i + g] - x[i - 1];
-      let d = dif / (g + 1);
-      while (x[i] == 0 || x[i] > 9000) {
-        x[i] = Number(x[i - 1]) + Number(d);
-        ++i;
-      }
-    }
-  }
+
   return x;
 }

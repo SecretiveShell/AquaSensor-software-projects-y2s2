85f60757658f12a6423a3cadbf1b420d6393da33
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-02-24 15:58:05 +0000

    add borders to studio

--- api-middleware/src/api_middleware/__init__.py
+++ api-middleware/src/api_middleware/__init__.py
@@ -4,7 +4,7 @@ from .app import app
 def main() -> None:
     from uvicorn import run
 
-    run(app, host="0.0.0.0", port=8000)
+    run(app, host="0.0.0.0", port=8001)
 
 
 if __name__ == "__main__":

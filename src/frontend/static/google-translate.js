5e9c1649c37e84ed007e52222f95f0e75c600a94
Connor2308 <c0027666@hallam.shu.ac.uk>
2025-03-26 10:09:12 +0000

    sidebar darkmode checkbox and translate(translate is still being worked on)

---
+++ src/frontend/static/google-translate.js
@@ -0,0 +1,3 @@
+function googlTranslateElementInit(){
+    new googlTranslateElementInit({pagelanguage: 'en', layout: googlTranslateElementInit.translate.translateElememt.InlineLayout.SIMPLE}, 'google_tranlate_element');
+}

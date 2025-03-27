ad8212c3fee78e1a4e282d59cf42131f176b62a4
finn-t-brady <finn.t.brady@gmail.com>
2025-03-27 11:54:03 +0000

    graph exists

---
+++ src/frontend/static/correlateBody.js
@@ -0,0 +1,163 @@
+var logged;
+await _isLogged().then((value)=>logged=value);
+if(!logged){
+  document.getElementById("warnings").innerHTML+="<p><em>No Authentication, please <a href=\"login?r=/correlate\">login</a> to use this page</em></p>";
+  Array.from(document.getElementsByClassName("authrequired")).forEach((arg)=>arg.setAttribute("disabled",""));
+}
+if(logged){
+  //
+  var chart=echarts.init(document.getElementById("chart-section"));
+
+
+  var level;
+  var flow;
+  let now=new Date();
+  await getLevel(now).then((value)=>level=value);
+  await getFlow(now).then((value)=>flow=value);
+  
+  var sm=DerwentWidthM*level/flow;
+  
+  //temporary values until I make the distance endpoint
+  //distance end point will probably be added post merge, as to avoid conflicts
+  var d1=5;
+  var d2=3;
+
+  let timeoff1=d1*sm;
+  let timeoff2=d2*sm;
+
+  let start=new Date();
+  start.setDate(now.getDate()-1);
+  start.setHours(23,0,0);
+  let till=new Date();
+  till.setDate(now.getDate()+1);
+  till.setHours(1,0,0);
+    
+  var data21={'time':[],'temperature':[],'dissolved_oxygen':[]};
+  var data1350={'time':[],'temperature':[],'dissolved_oxygen':[]};
+  var data13={'time':[],'temperature':[],'dissolved_oxygen':[]};
+  let r21 = dataRequest("941205",start,till); 
+  let r1350 = dataRequest("sensor044",start,till);
+  let r13 = dataRequest("sensor022",start,till);
+  await r21.then((value)=>value.json().then((value)=>
+		value["readings"].forEach((v)=>
+			{
+				data21['time'].push(new Date(v.datetime).addSeconds(timeoff1+timeoff2));
+				data21['temperature'].push(v.temperature);
+				data21['dissolved_oxygen'].push(v.dissolved_oxygen);
+			}
+		)
+  	)
+  );
+  await r1350.then((value)=>value.json().then((value)=>
+      value["readings"].forEach((v)=>
+			{
+				data1350['time'].push(new Date(v.datetime).addSeconds(timeoff2));
+				data1350['temperature'].push(v.temperature);
+				data1350['dissolved_oxygen'].push(v.dissolved_oxygen);
+			}
+		)
+  	)
+    );
+    await r13.then((value)=>value.json().then((value)=>
+		value["readings"].forEach((v)=>
+			{
+				data13['time'].push(new Date(v.datetime));
+				data13['temperature'].push(v.temperature);
+				data13['dissolved_oxygen'].push(v.dissolved_oxygen);
+			}
+		)
+    	)
+    );
+    var chartoptions={
+	    tootltip: {
+		    trigger: 'item',
+		    axisPointer:{type:'cross'}
+	    },
+	    xAxis: [
+		{
+		type: 'time',
+		boundaryGap:false,
+		name: "Sensor Readings",
+		nameGap: 0,
+		nameTextStyle: {
+			align:"center",
+			verticalAlign:"bottom"
+		}
+		}
+	    ],
+	    yAxis: [
+		{
+			type:"value"
+		},
+		{
+			type:"value"
+		},
+		{
+			type:"value"
+		},
+		{
+			type:"value"
+		},
+		{
+			type:"value"
+		},
+		{
+			type:"value"
+		}
+	    ],
+	    dataZoom: [
+		    {
+		    type:"inside"
+		    }
+	    ],
+	    legend: {
+		    data: ["Dissolved Oxygen","Temperature"]
+	    },
+	    series: [
+		{
+			name: "Dissolved Oxygen",
+			data: data21["time"].map((x,index)=>[x,data21["dissolved_oxygen"][index]]),
+			smooth:false,
+			symbol: "none",
+			type: "line"
+		},
+		{
+			name: "Temperature",
+			data: data21["time"].map((x,index)=>[x,data21["temperature"][index]]),
+			smooth:false,
+			symbol: "none",
+			type: "line"
+		},
+		{
+			name: "Dissolved Oxygen",
+			data: data1350["time"].map((x,index)=>[x,data1350["dissolved_oxygen"][index]]),
+			smooth:false,
+			symbol: "none",
+			type: "line"
+		},
+		{
+			name: "Temperature",
+			data: data1350["time"].map((x,index)=>[x,data21["temperature"][index]]),
+			smooth:false,
+			symbol: "none",
+			type: "line"
+		},
+		{
+			name: "Dissolved Oxygen",
+			data: data13["time"].map((x,index)=>[x,data13["dissolved_oxygen"][index]]),
+			smooth:false,
+			symbol: "none",
+			type: "line"
+		},
+		{
+			name: "Temperature",
+			data: data13["time"].map((x,index)=>[x,data13["temperature"][index]]),
+			smooth:false,
+			symbol: "none",
+			type: "line"
+		}
+	    ]
+    };
+    chart.setOption(chartoptions);
+}
+
---
+++ src/frontend/static/correlateHead.js
@@ -0,0 +1,69 @@
+//daily mean flow
+const flowApi = "https://environment.data.gov.uk/hydrology/id/measures/cad502d9-12f9-4449-a1e5-568b9f7d32e6-flow-m-86400-m3s-qualified/readings?";
+//daily min level
+const levelApi = "https://environment.data.gov.uk/hydrology/id/measures/cad502d9-12f9-4449-a1e5-568b9f7d32e6-level-min-86400-m-qualified/readings?";
+const DerwentWidthM = 6;
+/*
+flow=m3/s
+level=m
+width=m
+l*w/f=s/m
+
+distance=m
+s*m/m = s
+*/
+
+var warnList={'flowError':false,'levelError':false};
+
+async function getFlow(d){
+	let s=d.toISOString().substring(0,10);
+	let url=flowApi+"mineq-date="+s+"&maxeq-date="+s;
+	let r = await fetch(url);
+	let j = await r.json();
+	if(j["items"].length==0){
+		url=flowApi+"latest";
+		r= await fetch(url);
+		j= await r.json();
+		warnList['flowError']=true;
+	}
+	return j["items"][0]["value"];
+}
+async function getLevel(d){
+	let s=d.toISOString().substring(0,10);
+	let url=levelApi+"mineq-date="+s+"&maxeq-date="+s;
+	let r = await fetch(url);
+	let j = await r.json();
+	if(j["items"].length==0){
+		url=levelApi+"latest";
+		r= await fetch(url);
+		j= await r.json();
+		warnList['levelError']=true;
+	}
+	return j["items"][0]["value"];
+}
+async function parseWarnings(){
+	let warn=document.getElementById("warnings");
+	let c=0;
+	warn.innerHTML="";
+	if(warnList['flowError']){
+		warn.innerHTML+="<em>Issues retreiving requested flow rate, latest reading used instead</em>"
+		warnList['flowError']=false;
+		c++;
+	}
+	if(warnList['levelError']){
+		warn.innerHTML+="<em>Issues retreiving requested river level, latest reading used instead</em>"
+		warnList['levelError']=false;
+		c++;
+	}
+	return c;
+}
+
+async function dataRequest(id,start,till){
+	console.log("api/v1/sensors/"+id+"/readings?start_date="+start.toISOString()+"&end_date="+till.toISOString());
+	return fetch("api/v1/sensors/"+id+"/readings?start_date="+start.toISOString()+"&end_date="+till.toISOString(),{
+		headers: {
+			Accept: "appication/json",
+			"AquaSensor-Login-Token": window.sessionStorage["AquaSensorToken"]
+		}
+	});
+}
